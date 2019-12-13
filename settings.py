a729462662b82da675581766f0e71ddead71a832
Shuo Qi <MagicianQi@users.noreply.github.com>
2019-12-13 14:14:23 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # Model
-FACE_DETECTION_SERVER_URL = 'http://172.29.100.1:6902/v1/models/face_detection:predict'
-FACE_VERIFICATION_SERVER_URL = 'http://172.29.100.1:8541/v1/models/face_verification:predict'
+FACE_DETECTION_SERVER_URL = 'http://face_detection:6902/v1/models/face_detection:predict'
+FACE_VERIFICATION_SERVER_URL = 'http://face_verification:8541/v1/models/face_verification:predict'
 FACE_CALIBRATION_MODEL_PATH = 'models/shape_predictor_68_face_landmarks.dat'
 FACE_DETECTION_THRESHOLD = 0.6
 IMAGE_SIZE = 160
